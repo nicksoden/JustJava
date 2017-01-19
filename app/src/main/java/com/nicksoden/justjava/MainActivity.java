9a03ae4eef5e614a4971d99dd361382114457102
nicksoden <ourhomegalaxy@gmail.com>
2017-01-18 19:01:25 -0500

    This is everything up to styles lesson

--- app/src/main/java/com/nicksoden/justjava/MainActivity.java
+++ app/src/main/java/com/nicksoden/justjava/MainActivity.java
@@ -5,18 +5,22 @@
  * package com.example.android.justjava;
  */
 package com.nicksoden.justjava;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,40 +32,98 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank you!";
+        /*Creates whipped cream Checkbox object*/
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        EditText editText = (EditText) findViewById(R.id.edit_text_name);
+        String editTextName = editText.getText().toString();
+
+        Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream + editTextName);
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, editTextName);
         displayMessage(priceMessage);
 
+        /*displayMessage(createOrderSummary(price));
+        *
+        * this is better than the above code
+        */
     }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
-
 
     /**
      * Calculates the price of the order.
      *
      * @return total price
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
+    private int calculatePrice(boolean whippedCream, boolean chocolate) {
+        int multiplier = 5;
+        /*
+        change this to ...
+        return quantity * 5;
+        to be more precise
+        */
+
+        if (whippedCream) {
+            multiplier += 1;
+        }
+        if (chocolate) {
+            multiplier += 3;
+            Log.v("MainActivity", "multiplier");
+        }
+        int price = quantity * multiplier;
         return price;
+    }
+
+    /**
+     * Create summary of the order
+     * @param price
+     * @param hasWhippedCream is whether the user has whipped cream or not
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String editTextName){
+        String priceMessage = getString(R.string.persons) + editTextName;
+        priceMessage += "\n" + getString(R.string.added_whipped_cream) + hasWhippedCream;
+        priceMessage += "\n" + getString(R.string.added_chocolate) + hasChocolate;
+        priceMessage += "\n" + getString(R.string.quantity_ordered)+ quantity;
+        priceMessage +=  "\n" + getString(R.string.total_price) + price;
+        priceMessage +=  "\n" + getString(R.string.thank_you);
+        return priceMessage;
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
@@ -72,12 +134,5 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
 }
